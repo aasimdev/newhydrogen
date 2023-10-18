0b5cfad04ad01fdc9ac4366629678846d94461d7
Ahtisham101 <ahtisham_101@outlook.com>
2023-10-18 16:09:22 +0500

    feat: add new shorts

--- data/podcast-data.php
+++ data/podcast-data.php
@@ -1,6 +1,276 @@
 <?php
 $videos = [
     [
+        'title' => "How Hydrogen is Revolutionizing the Transportation Industry",
+        'videoID' => "PVi70lFkaTk",
+        'category' => "short-video",
+        "display" => true
+    ],
+    [
+        'title' => "California Invested $106M into Better Fuel Sources",
+        'videoID' => "09ux-nmmKRg",
+        'category' => "short-video",
+        "display" => true
+    ],
+    [
+        'title' => "The Staggering Impact of Maritime Shipping on the Environment!",
+        'videoID' => "euKREzOmBZ4",
+        'category' => "short-video",
+        "display" => true
+    ],
+    [
+        'title' => "How to Make Renewable Energy in Bad Weather",
+        'videoID' => "mePTGm3NqM0",
+        'category' => "short-video",
+        "display" => true
+    ],
+    [
+        'title' => "The Energy of The Future: Hydrogen Power Explained",
+        'videoID' => "_p0AtMQZAXA",
+        'category' => "short-video",
+        "display" => true
+    ],
+    [
+        'title' => "Sending a Toyota Mirai Around the World 24x",
+        'videoID' => "NVe9bhO3sCg",
+        'category' => "short-video",
+        "display" => true
+    ],
+    [
+        'title' => "First Place in the Race Towards Renewable Energy",
+        'videoID' => "q56-8IFRX8M",
+        'category' => "short-video",
+        "display" => true
+    ],
+    [
+        'title' => "Stanford's Innovative Approach to the Hydrogen Economy!",
+        'videoID' => "G5Ncugka_cE",
+        'category' => "short-video",
+        "display" => true
+    ],
+    [
+        'title' => "Transportation at it's fastest and most efficient.",
+        'videoID' => "CWrDfhkoBIk",
+        'category' => "short-video",
+        "display" => true
+    ],
+    [
+        'title' => "The technology that powers tomorrow is being made today at NewHydrogen.",
+        'videoID' => "u9jnJ_3ARSQ",
+        'category' => "short-video",
+        "display" => true
+    ],
+    [
+        'title' => "The Skittles Approach to Carbon Neutrality!",
+        'videoID' => "ICEjaymJVOo",
+        'category' => "short-video",
+        "display" => true
+    ],
+    [
+        'title' => "The Rainbow of Hydrogen Production!",
+        'videoID' => "BtNGUh9rLHk",
+        'category' => "short-video",
+        "display" => true
+    ],
+    [
+        'title' => "How can we make grocery shopping accessible again?",
+        'videoID' => "bHOkhCQqXS4",
+        'category' => "short-video",
+        "display" => true
+    ],
+    [
+        'title' => "What the public thinks about grocery prices!",
+        'videoID' => "oahj2M-GITE",
+        'category' => "short-video",
+        "display" => true
+    ],
+    [
+        'title' => "Watering Down Your Power Bill!",
+        'videoID' => "zNYXBBkPwW0",
+        'category' => "short-video",
+        "display" => true
+    ],
+    [
+        'title' => "How Hydrogen Can Replace Carbon Dioxide.",
+        'videoID' => "24OOLtsGbQI",
+        'category' => "short-video",
+        "display" => true
+    ],
+    [
+        'title' => "V1 WITH MUSIC Pumpkin Patch 1",
+        'videoID' => "a6sVB7eOXMs",
+        'category' => "short-video",
+        "display" => true
+    ],
+    [
+        'title' => "How is cheap hydrogen helping your local farmer's market?",
+        'videoID' => "kmBsIlRz230",
+        'category' => "short-video",
+        "display" => true
+    ],
+    [
+        'title' => "How Thermoloop is Changing the Renewable Game!",
+        'videoID' => "5BEf0J9Y-yY",
+        'category' => "short-video",
+        "display" => true
+    ],
+    [
+        'title' => "The Green-Hydrogen Economy is Ready to Go!",
+        'videoID' => "Fak2jxnpR9Q",
+        'category' => "short-video",
+        "display" => true
+    ],
+    [
+        'title' => "How are Hydrogen Fertilizers Changing Agriculture?",
+        'videoID' => "4XJb_jRRiO8",
+        'category' => "short-video",
+        "display" => true
+    ],
+    [
+        'title' => "Let's Start a Discussion about Green Hydrogen. We'll Go First!",
+        'videoID' => "GCFQE-mtew8",
+        'category' => "short-video",
+        "display" => true
+    ],
+    [
+        'title' => "Never Wasting a Watt!",
+        'videoID' => "yUBt3113jr4",
+        'category' => "short-video",
+        "display" => true
+    ],
+    [
+        'title' => "Hydrogen Powered Aircrafts are Coming Soon!",
+        'videoID' => "JaKH85UcxXc",
+        'category' => "short-video",
+        "display" => true
+    ],
+    [
+        'title' => "How is Hydrogen Critical to American Agriculture?",
+        'videoID' => "81UDvF72fYo",
+        'category' => "short-video",
+        "display" => true
+    ],
+    [
+        'title' => "How Hydrogen Helps Our Farmers Every Day",
+        'videoID' => "6F-m-N6cuQs",
+        'category' => "short-video",
+        "display" => true
+    ],
+    [
+        'title' => "Hydrogen Revolution: Paving the Way to Sustainable Industries",
+        'videoID' => "5lQWmZI0Gt8",
+        'category' => "short-video",
+        "display" => true
+    ],
+    [
+        'title' => "The #1 Element",
+        'videoID' => "HoAKGK6C9ZQ",
+        'category' => "short-video",
+        "display" => true
+    ],
+    [
+        'title' => "Something to think about this summer.",
+        'videoID' => "Z8Xg2LfuETk",
+        'category' => "short-video",
+        "display" => true
+    ],
+    [
+        'title' => "Big Power in a Small Package!",
+        'videoID' => "cd-RQ27GvTA",
+        'category' => "short-video",
+        "display" => true
+    ],
+    [
+        'title' => "The Fast and the Futuristic",
+        'videoID' => "Rp7ViESbRxs",
+        'category' => "short-video",
+        "display" => true
+    ],
+    [
+        'title' => "Energy without the climate cost",
+        'videoID' => "Qdnri3_Xm2k",
+        'category' => "short-video",
+        "display" => true
+    ],
+    [
+        'title' => "With zero emissions and far better utilization, the future is green, not grey.",
+        'videoID' => "_1pUYS6LBLg",
+        'category' => "short-video",
+        "display" => true
+    ],
+    [
+        'title' => "Hydrogen production is the bedrock of many industries, but it has been costly in the past.",
+        'videoID' => "vhFcUfVXcHM",
+        'category' => "short-video",
+        "display" => true
+    ],
+    [
+        'title' => "The Hydrogen Revolution",
+        'videoID' => "v_OYIwdw3Mw",
+        'category' => "short-video",
+        "display" => true
+    ],
+    [
+        'title' => "Clean energy benefits everyone, both on the ground and in the air.",
+        'videoID' => "Gy7oKQgZ9K4",
+        'category' => "short-video",
+        "display" => true
+    ],
+    [
+        'title' => "The Hydrogen Rainbow",
+        'videoID' => "keWqCbP4V50",
+        'category' => "short-video",
+        "display" => true
+    ],
+    [
+        'title' => "Progress will not stop for anybody; neither will we.",
+        'videoID' => "ZpQkip0-_Xo",
+        'category' => "short-video",
+        "display" => true
+    ],
+    [
+        'title' => "Green hydrogen has 60 countries' worth of support of a hydrogen-powered world.",
+        'videoID' => "yp_1Y2IhYag",
+        'category' => "short-video",
+        "display" => true
+    ],
+    [
+        'title' => "Hydrogen is the foundation of most industries today.",
+        'videoID' => "E16fxm0fzHU",
+        'category' => "short-video",
+        "display" => true
+    ],
+    [
+        'title' => "Excitement for the Hydrogen Revolution is growing- as it should!",
+        'videoID' => "miUsogZ4B4s",
+        'category' => "short-video",
+        "display" => true
+    ],
+    [
+        'title' => "Our mission is to provide the world with THE CHEAPEST HYDROGEN.",
+        'videoID' => "3VNfr-e2TjA",
+        'category' => "short-video",
+        "display" => true
+    ],
+    [
+        'title' => "The time for energy equity is now.",
+        'videoID' => "-jWwEtNvSfk",
+        'category' => "short-video",
+        "display" => true
+    ],
+    [
+        'title' => "Our planet is begging us to stop producing and releasing fossil fuels into the atmosphere.",
+        'videoID' => "T4LY5CSpwgE",
+        'category' => "short-video",
+        "display" => true
+    ],
+    [
+        'title' => "We are on a mission to provide the world's cheapest green hydrogen.",
+        'videoID' => "FVnrYO_lVwI",
+        'category' => "short-video",
+        "display" => true
+    ],
+    [
         'title' => "The best way to go about changing the world is to make it a better and safer place to live!",
         'videoID' => "43rbZ319DH4",
         'category' => "short-video",
